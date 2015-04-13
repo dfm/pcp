5461f1eb91d9e25cc8620fb884662fe7fe7b4d05
Dan F-M <danfm@nyu.edu>
2015-04-13 16:12:42 -0400

    initial

---
+++ demo.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+import numpy as np
+from PIL import Image
+
+from pcp import pcp
+
+
+def bitmap_to_mat(bitmap_seq):
+    """from blog.shriphani.com"""
+    matrix = []
+    shape = None
+    for bitmap_file in bitmap_seq:
+        img = Image.open(bitmap_file).convert("L")
+        if shape is None:
+            shape = img.size
+        assert img.size == shape
+        img = np.array(img.getdata())
+        matrix.append(img)
+    return np.array(matrix), shape[::-1]
+
+
+def do_plot(img, shape):
+    pl.clf()
+    pl.imshow(img.reshape(shape), cmap="gray", interpolation="nearest")
+    return pl.gcf()
+
+
+if __name__ == "__main__":
+    import sys
+    import glob
+    import matplotlib.pyplot as pl
+
+    if "--test" in sys.argv:
+        M = (10*np.ones((10, 10))) + (-5 * np.eye(10))
+        L, S, svd = pcp(M)
+        assert np.allclose(M, L + S), "Failed"
+        print("passed")
+        sys.exit(0)
+
+    M, shape = bitmap_to_mat(glob.glob("test_data/Escalator/*.bmp")[:300])
+    fig = do_plot(M[0], shape)
+    fig.savefig("plot-initial.png")
+
+    L, S, (u, s, v) = pcp(M, maxiter=1000, verbose=True, delta=1e-4)
+    fig = do_plot(L[0], shape)
+    fig.savefig("plot-L.png")
+
+    fig = do_plot(S[0], shape)
+    fig.savefig("plot-S.png")
+
+    fig = do_plot(v[0], shape)
+    fig.savefig("plot-eigen.png")
---
+++ pcp.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+
+References:
+- http://perception.csl.illinois.edu/matrix-rank/Files/RPCA_JACM.pdf
+- http://blog.shriphani.com/2013/12/18/
+    robust-principal-component-pursuit-background-matrix-recovery/
+
+"""
+
+from __future__ import division, print_function
+
+__all__ = ["pcp"]
+
+import logging
+import numpy as np
+from scipy.sparse.linalg import svds
+
+
+def pcp(M, delta=1e-6, mu=None, maxiter=500, verbose=False):
+    shape = M.shape
+
+    # Initialize the tuning parameters.
+    lam = 1.0 / np.sqrt(np.max(shape))
+    if mu is None:
+        mu = 0.25 * np.prod(shape) / np.sum(np.abs(M))
+
+    # Convergence criterion.
+    norm = np.sum(M ** 2)
+
+    # Iterate.
+    i = 0
+    rank = np.min(shape)
+    S = np.zeros(shape)
+    Y = np.zeros(shape)
+    while i < max(maxiter, 1):
+        # SVD step.
+        if rank >= np.min(shape):
+            u, s, v = np.linalg.svd(M - S + Y / mu, full_matrices=False)
+        else:
+            u, s, v = svds(M - S + Y / mu, k=rank, tol=1.0/mu)
+
+            # The sparse implementation returns the SVs in reverse order.
+            u, s, v = u[:, ::-1], s[::-1], v[::-1, :]
+
+        s = shrink(s, 1.0 / mu)
+        rank = np.sum(s > 0.0)
+        u, s, v = u[:, :rank], s[:rank], v[:rank, :]
+        L = np.dot(u, np.dot(np.diag(s), v))
+
+        # Shrinkage step.
+        S = shrink(M - L + Y / mu, lam / mu)
+
+        # Lagrange step.
+        step = M - L - S
+        Y += mu * step
+
+        # Check for convergence.
+        err = np.sqrt(np.sum(step ** 2) / norm)
+        if verbose:
+            print("Iteration {0}: error={1:.3e}, rank={2:d}"
+                  .format(i, err, np.sum(s > 0)))
+        if err < delta:
+            break
+        i += 1
+
+    if i >= maxiter:
+        logging.warn("convergence not reached in pcp")
+    return L, S, (u, s, v)
+
+
+def shrink(M, tau):
+    sgn = np.sign(M)
+    S = np.abs(M) - tau
+    S[S < 0.0] = 0.0
+    return sgn * S
