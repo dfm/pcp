3501bedc6025d838d0fedac51d50637a1504253f
Dan F-M <danfm@nyu.edu>
2015-04-13 21:07:09 +0000

    missing data

--- pcp.py
+++ pcp.py
@@ -20,13 +20,20 @@ import numpy as np
 from scipy.sparse.linalg import svds
 
 
-def pcp(M, delta=1e-6, mu=None, maxiter=500, verbose=False):
+def pcp(M, delta=1e-6, mu=None, maxiter=500, verbose=False, missing_data=True):
     shape = M.shape
+    if missing_data:
+        missing = ~(np.isfinite(M))
+        M[missing] = 0.0
+    else:
+        missing = np.zeros_like(M, dtype=bool)
 
     # Initialize the tuning parameters.
     lam = 1.0 / np.sqrt(np.max(shape))
     if mu is None:
         mu = 0.25 * np.prod(shape) / np.sum(np.abs(M))
+        if verbose:
+            print("mu = {0}".format(mu))
 
     # Convergence criterion.
     norm = np.sum(M ** 2)
@@ -56,13 +63,14 @@ def pcp(M, delta=1e-6, mu=None, maxiter=500, verbose=False):
 
         # Lagrange step.
         step = M - L - S
+        step[missing] = 0.0
         Y += mu * step
 
         # Check for convergence.
         err = np.sqrt(np.sum(step ** 2) / norm)
         if verbose:
-            print("Iteration {0}: error={1:.3e}, rank={2:d}"
-                  .format(i, err, np.sum(s > 0)))
+            print("Iteration {0}: error={1:.3e}, rank={2:d}, nnz={3:d}"
+                  .format(i, err, np.sum(s > 0), np.sum(S > 0)))
         if err < delta:
             break
         i += 1
